940ebd9ce08d2f6a11cb255759a49ca60ac05877
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-05-27 15:27:43 +0430

    Get Api

---
+++ src/Functions/GetApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+//BASE API
+const BASE_API = "https://fakestoreapi.com"
+
+const getApi = async () => {
+    const result = await axios.get(`${BASE_API}/products`);
+    return result.data
+}
+
+export { getApi }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Component-style/index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( <BrowserRouter> <App/> </BrowserRouter> );
 
